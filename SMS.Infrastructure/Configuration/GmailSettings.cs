98c009fbe0c668940c3c52cacabc370b0b7ed6cd
Arulappan <rayaarul@gmail.com>
2025-11-27 21:13:05 +0530

    Refinement in the appsettings.json for the serilog

--- SMS.Infrastructure/Configuration/GmailSettings.cs
+++ SMS.Infrastructure/Configuration/GmailSettings.cs
@@ -4,9 +4,9 @@
     {
         public const string SettingsKey = "GmailSettings";
 
-        public string SmtpHost { get; set; } = string.Empty;
-        public int SmtpPort { get; set; }
-        public string FromAddress { get; set; } = string.Empty;
-        public string AppPassword { get; set; } = string.Empty;
+        public required string SmtpHost { get; init; }
+        public required int SmtpPort { get; init; }
+        public required string FromAddress { get; init; }
+        public required string AppPassword { get; init; }
     }
 }
